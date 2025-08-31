45132529f92ce227e7d571020d696287d6846d0a
Utsav Shresth <shresthutsav1012@gmail.com>
2025-08-31 23:02:16 +0530

    Time: 4 ms (36.63%) | Memory: 15.7 MB (35.42%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    void backtrack(string current, int open, int close, int n, vector<string>& result) {
+       
+        if (current.size() == 2 * n) {
+            result.push_back(current);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(current + "(", open + 1, close, n, result);
+        }
+
+     
+        if (close < open) {
+            backtrack(current + ")", open, close + 1, n, result);
+        }
+    }
+
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        backtrack("", 0, 0, n, result);
+        return result;
+    }
+};
