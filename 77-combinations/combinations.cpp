4b74bbaeb8765f05f7f4098c80864912f2dab1c0
Utsav Shresth <shresthutsav1012@gmail.com>
2025-09-14 19:32:47 +0530

    Time: 71 ms (42.19%) | Memory: 110 MB (27.57%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+
+void sol( int n,int k,int v,vector<int> &a,vector<vector<int>> &ans){
+                    if(a.size()==k){
+                        ans.push_back(a);
+                        return;
+                    }
+                  if(v>n) {
+                    return;
+                  } 
+      a.push_back(v);
+        
+        sol(n,k,v+1,a,ans);
+        a.pop_back();
+       
+        sol(n,k,v+1,a,ans);
+
+
+
+
+
+}
+    vector<vector<int>> combine(int n, int k) {
+         
+
+          vector<int>a;
+          vector<vector<int>> ans;
+         
+          int v=1;
+          sol(n,k,v,a,ans);
+          return ans;
+       
+        
+    }
+};
